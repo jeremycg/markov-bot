f94520cf897806c93fac8ed1e4a7cd31ae6d7529
Jeremy Gray <jeremycgray@gmail.com>
2016-09-27 22:00:04 -0400

    speedup - uses panda to read csv

--- bot.py
+++ bot.py
@@ -3,36 +3,41 @@ from twython import Twython
 import sys
 import csv
 import random
+import pandas as pd
 
 #input is a file generated from:
 #http://www.ncbi.nlm.nih.gov/pubmed?term=%22PloS%20one%22%5BJournal%5D
 #exported as .csv
 #easy to modify for any other journal
 
-traindict={}
-startingwords=[]
-
 twitter = Twython("API KEY",
                   "API SECRET",
                   "ACCESS TOKEN",
                   "YOUR ACCESS TOKEN SECRET")
 
+def onetitle(x,traindict, startingwords):
+    words = x.split(' ')
+    if len(words) < 4:
+        return
+    else:
+        for wordindex in range(len(words)-2):
+            if (words[wordindex],words[wordindex+1]) not in traindict:
+                traindict[(words[wordindex],words[wordindex+1])] = [words[wordindex+2]]
+            else:
+                traindict[(words[wordindex],words[wordindex+1])].append(words[wordindex+2])
+        if (words[-2],words[-1]) not in traindict:
+            traindict[(words[-2],words[-1])] = [False]
+        else:
+            traindict[(words[-2],words[-1])].append(False)
+        startingwords.append((words[0],words[1]))
+        return
+    
 def makedict(file):
-  with open(file, 'rt',encoding="utf-8") as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-     for row in spamreader:
-         words=row[0].split(sep=" ")
-         if len(words)<4:
-             continue
-         for wordindex in range(len(words)-2):
-             if (words[wordindex],words[wordindex+1]) not in traindict:
-                    traindict[(words[wordindex],words[wordindex+1])] = list()
-             traindict[(words[wordindex],words[wordindex+1])].append(words[wordindex+2])
-         if (words[-2],words[-1]) not in traindict:
-             traindict[(words[-2],words[-1])] = list()
-         traindict[(words[-2],words[-1])].append(False)
-         startingwords.append((words[0],words[1]))
-  return(traindict,startingwords)
+    x = list(pd.read_csv(file, error_bad_lines=False, usecols = ['Title']).index)
+    traindict={}
+    startingwords=[]
+    [onetitle(i, traindict, startingwords) for i in x]
+    return traindict,startingwords
 
 
 def maketweet(dicttouse,startingwords):
@@ -44,13 +49,13 @@ def maketweet(dicttouse,startingwords):
     sentence=sentence+[chosen[0]]
     return(" ".join(sentence))
 
+if __name__ == "__main__":
+    a, b = makedict(sys.argv[1])
 
-a, b = makedict(sys.argv[1])
-
-tweeted=0
-while tweeted==0:
-    totweet=maketweet(a,b)
-    if len(totweet)<141:
-        twitter.update_status(status=totweet)
-        print(totweet)
-        tweeted=1
+    tweeted=0
+    while tweeted==0:
+        totweet=maketweet(a,b)
+        if len(totweet)<141:
+            twitter.update_status(status=totweet)
+            print(totweet)
+            tweeted=1
